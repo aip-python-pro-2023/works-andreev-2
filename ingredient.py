940e8989270e3fbc5d2e0db4cd7573d56b3a0cec
Nikolay Andreev <bakasaru@list.ru>
2023-12-18 19:08:33 +0300

    feat: add getter/setter and property examples

--- ingredient.py
+++ ingredient.py
@@ -10,24 +10,43 @@ class Ingredient:
 
     # Конструктор, вызывается при создании объекта класса Ingredient
     def __init__(self, name, description, ingredient_type, picture, calories, proteins, fats, carbohydrates):
-        self.id = Ingredient.current_id
+        self.__id = Ingredient.current_id
         Ingredient.current_id += 1
-        self.name = name
-        self.description = description
-        self.type = ingredient_type
-        self.picture = picture
-        self.calories = calories
-        self.proteins = proteins
-        self.fats = fats
-        self.carbohydrates = carbohydrates
+        self.__name = name
+        self.__description = description
+        self.__type = ingredient_type
+        self.__picture = picture
+        self.__calories = calories
+        self.__proteins = proteins
+        self.__fats = fats
+        self.__carbohydrates = carbohydrates
+
+    # Геттер
+    def get_fats(self):
+        return self.__fats
+
+    # Сеттер
+    def set_fats(self, new_fats):
+        if 0 <= new_fats <= 100:
+            self.__fats = new_fats
+
+    @property
+    def fats(self):
+        return self.__fats
+
+    @fats.setter
+    def fats(self, value):
+        print('Property setter')
+        if 0 <= value <= 100:
+            self.__fats = value
 
     # Превращаем объект в строчку
     def __str__(self):
-        return f'Ingredient(id={self.id}, name={self.name}, calories={self.calories}, proteins={self.proteins}, fats={self.fats}, carbohydrates={self.carbohydrates})'
+        return f'Ingredient(id={self.__id}, name={self.__name}, calories={self.__calories}, proteins={self.__proteins}, fats={self.__fats}, carbohydrates={self.__carbohydrates})'
 
     # Проверка на равенство
     def __eq__(self, other):
-        return isinstance(other, Ingredient) and self.name == other.name
+        return isinstance(other, Ingredient) and self.__name == other.__name
 
     @staticmethod
     def create_from_input():
@@ -44,14 +63,22 @@ class Ingredient:
 
 
 carrot = Ingredient('Морковь', 'Вкусный и полезный овощ', 'Овощ', 'https://vkustorg.com/image/cache/Ovochi_frukty/1460024087-500x500.jpg', 150, 70, 10, 20)
+# carrot.__fats = 5000
 print(carrot)
-print(carrot.name)
-print(carrot.description)
+print(carrot.get_fats())
+carrot.set_fats(0)
+print(carrot)
+
+# carrot.__fats += 10
+
+# Через геттер и сеттер
+carrot.set_fats(carrot.get_fats() + 120)
+# Через свойства
+carrot.fats += 120
+print(carrot.fats)
 
 sugar = Ingredient('Сахар', 'Сладкая вещь, с которой не стоит перебарщивать', 'Бакалея', 'https://centrstomatologii.ru/upload/resize_cache/webp/iblock/8b9/fe1rhhjon2lrtjfaqjoejc14elz4xk8s/1300_850_1/wide_4_3_821a7b2af4c4914631b84d86a6cafaad.webp', 250, 0, 0, 100)
 print(sugar)
-print(sugar.name)
-print(sugar.description)
 
 # Не делайте так
 # print(Ingredient.current_id)
